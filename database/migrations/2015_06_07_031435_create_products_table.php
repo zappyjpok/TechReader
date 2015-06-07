6690bedf6926ed30a76618b9ece4bb4bc5f26401
zappyjpok <potentia2020@yahoo.com>
2015-06-07 13:20:41 +1000

    Update: renamed products table

--- database/migrations/2015_06_07_031435_create_products_table.php
+++ database/migrations/2015_06_07_031435_create_products_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('product', function(Blueprint $table) {
+        Schema::create('products', function(Blueprint $table) {
             $table->increments('id');
             $table->string('proTitle');
             $table->string('proAuthor');
@@ -32,6 +32,6 @@ class CreateProductTable extends Migration
      */
     public function down()
     {
-        Schema::drop('product');
+        Schema::drop('products');
     }
 }
