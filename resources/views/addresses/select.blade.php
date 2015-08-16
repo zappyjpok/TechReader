a3f51665ee2fed1b35bdac5688a7eeccd5a86a9c
zappyjpok <potentia2020@yahoo.com>
2015-08-16 14:16:18 +1000

    the select page has the panel for the css added

--- resources/views/addresses/select.blade.php
+++ resources/views/addresses/select.blade.php
@@ -10,44 +10,55 @@
 
 @section('content')
 
-    @include('addresses._partials._userInfo', ['user' => $user])
-
-    <!-- Create a new address link -->
-    <section class="row display-user-info top-buffer-20">
-        <h4> Need to create a new address </h4>
-        <div class="row">
-            <a class="btn btn-primary btn-lg"
-               href="{{ action('AddressesController@create', $user->name) }}"> Create a new Address </a>
-        </div>
-    </section>
-
-    <!-- Select Address -->
-    <section class="row display-user-info top-buffer-20">
-        <h4> Please select an address or create a new one </h4>
-        <div class="row">
-            {!! Form::open([
-            'action' => ['OrdersController@addAddress', $user->id]
-            ])
-            !!}
-            @foreach($user->addresses as $address)
-                <div class="col-md-3 col-sm-3">
-                    {!! Form::radio('address_id', $address->id) !!}
-                    <p class="address-box">
-                        {{ $address->address }} <br />
-                        {{ $address->state }}, {{ $address->city }}  <br />
-                        {{ $address->postal_code }}
-                    </p>
-                    <a class="btn btn-primary"
-                       href="{{ action('AddressesController@edit', $address->id) }}"> Edit Address </a>
-                </div>
-            @endforeach
-            <div class="row">
-                <div class="col-md-12 top-buffer-20">
-                    {!! Form::submit('Continue', ['class' => 'btn btn-primary', 'name' => 'Submit']) !!}
-                </div>
+    <div class="row top-buffer-20">
+        <div class="col-md-7">
+            <div class="panel panel-default">
+                <article class="panel-heading">
+                    <h4> Please Select your address  </h4>
+                </article>
+                <article class="panel-body">
+                    <section class="row">
+                        <div class="col-md-offset-1">
+                            <h5> Need to create a new address </h5>
+                            <div>
+                                <a class="btn btn-primary btn-lg"
+                                   href="{{ action('AddressesController@create', $user->name) }}"> Create a new Address </a>
+                            </div>
+                        </div>
+                    </section>
+                    <section class="row top-buffer-10">
+                        <div class="col-md-offset-1">
+                            <div class="row">
+                                {!! Form::open([
+                                'action' => ['OrdersController@addAddress', $user->id]
+                                ])
+                                !!}
+                                @foreach($user->addresses as $address)
+                                    <div class="col-md-3 col-sm-3">
+                                        {!! Form::radio('address_id', $address->id) !!}
+                                        <p class="address-box">
+                                            {{ $address->address }} <br />
+                                            {{ $address->state }}, {{ $address->city }}  <br />
+                                            {{ $address->postal_code }}
+                                        </p>
+                                        <a class="btn btn-primary"
+                                           href="{{ action('AddressesController@edit', $address->id) }}"> Edit Address </a>
+                                    </div>
+                                @endforeach
+                                <div class="row">
+                                    <div class="col-md-12 top-buffer-20">
+                                        {!! Form::submit('Continue', ['class' => 'btn btn-primary', 'name' => 'Submit']) !!}
+                                    </div>
+                                </div>
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+                    </section>
+                </article>
             </div>
-            {!! Form::close() !!}
         </div>
-    </section>
-
+        <div class="col-md-5">
+            @include('addresses._partials._userInfo', ['user' => $user])
+        </div>
+    </div>
 @endsection
